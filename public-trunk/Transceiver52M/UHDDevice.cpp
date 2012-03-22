ttsou/openbts-p2.8

transceiver, uhd: output timestamps on monontonic error
The "Loss of monotonic time" error occurs when a timestamp
arrives from the device that is earlier in time than the
previous timestamp. The output is an ALERT level message
generally accompanied by a transmit side timeout from the
device.

UHD: Loss of monotonic time
UHD: Loss of monotonic time
UHD: Device send timed out

Add to the error description the timestamp values that
generated the error and output with ALERT rather than
ERR log.

Signed-off-by: Thomas Tsou <ttsou@vt.edu>

git-svn-id: http://wush.net/svn/range/software/public/openbts/trunk@3307 19bc5d8c-e614-43d4-8b26-e1612bc8e597

Transceiver52M/UHDDevice.cpp
@@ -597,8 +597,8 @@ int uhd_device::check_rx_md_err(uhd::rx_metadata_t &md, ssize_t num_smpls)
 	// Monotonicity check
 	if (ts < prev_ts) {
 		LOG(ALERT) << "UHD: Loss of monotonic time";
-		LOG(ERR) << "UHD: Current time: " << ts.get_real_secs();
-		LOG(ERR) << "UHD: Previous time: " << prev_ts.get_real_secs();
+		LOG(ALERT) << "Current time: " << ts.get_real_secs() << ", " 
+			   << "Previous time: " << prev_ts.get_real_secs();
 		return ERROR_TIMING;
 	} else {
 		prev_ts = ts;

0x7678/openbts-uhd

public-trunk/Transceiver52M/UHDDevice.cpp
@@ -596,9 +596,9 @@ int uhd_device::check_rx_md_err(uhd::rx_metadata_t &md, ssize_t num_smpls)
 
 	// Monotonicity check
 	if (ts < prev_ts) {
-		LOG(ALARM) << "UHD: Loss of monotonic: " << ts.get_real_secs();
-		LOG(ERROR) << "UHD: Current time: " << ts.get_real_secs();
-		LOG(ERROR) << "UHD: Previous time: " << prev_ts.get_real_secs();
+		LOG(ALARM) << "UHD: Loss of monotonic time";
+		LOG(ALARM) << "Current time: " << ts.get_real_secs() << ", " 
+			   << "Previous time: " << prev_ts.get_real_secs();
 		return ERROR_TIMING;
 	} else {
 		prev_ts = ts;